f821527da3154427952dd4e26a1db3daf4302472
Ishan <ishan.sandhu3121@gmail.com>
2025-07-22 22:57:24 -0700

    got done with about section

--- src/About.jsx
+++ src/About.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import ProfileCardComponent from "./helper/profileCard";
 import "./About.css";
 import avatarImg from './assets/img2.png';
+import miniAvatarImg from './assets/github.webp';
+import { useRef } from 'react';
+import VariableProximity from './helper/variableProx';
+// import DecryptedText from "./helper/DecryptText";
+
 
 export default function About() {
+  const containerRef = useRef(null);
     return (
 
-        <div className="about-container">
-  <div className="about-content">
+  <div className="about-container" id="about">
+  <div className="about-content" style={{marginTop: '10px'}}>
     <h1>About Me</h1>
   </div>
   <div className="about-row">
@@ -19,16 +25,28 @@ export default function About() {
       contactText="GitHub"
       showBehindGradient={true}
       avatarUrl="src/assets/img1.jpeg"
-      miniAvatarUrl={avatarImg}
+      miniAvatarUrl={miniAvatarImg}
       showUserInfo={true}
       enableTilt={true}
       enableMobileTilt={false}
-      onContactClick={() => console.log('Contact clicked')}
+      onContactClick={() => window.open('https://github.com/xIshanSandhux', '_blank')}
     />
     <div className="about-side-text">
-      <p>
-        {/* Your text here */}
-        Hi! I'm Ishan, a passionate software engineer with experience in building modern web applications. I love working with React, Node.js, and exploring new technologies.
+      <p> 
+      <div
+ref={containerRef}
+style={{position: 'relative', fontSize: '2rem', fontWeight: 'semibold'}}
+>        
+         <VariableProximity
+        label={"Hi! I'm Ishan, a 4th year software engineering student at University of Victoria (UVic) who loves to code and build cool stuff."}
+        className={'variable-proximity-demo'}
+        fromFontVariationSettings="'wght' 400, 'opsz' 9"
+        toFontVariationSettings="'wght' 1000, 'opsz' 40"
+        containerRef={containerRef}
+        radius={130}
+        falloff='gaussian'
+        />
+        </div>
       </p>
     </div>
   </div>
--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
     <Navbar />
     <Hero />
-    <About />
+    <About id="about" />
     </>
   )
 }
